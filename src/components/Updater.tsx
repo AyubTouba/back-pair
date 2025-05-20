873d1516962c5ef09b469b9bad9086b59cf30a84
Ayoub Touba <ayubtouba@gmail.com>
2025-05-20 21:21:32 +0200

    Fix updater

--- src/components/Updater.tsx
+++ src/components/Updater.tsx
@@ -7,56 +7,53 @@ import { Progress } from "./ui/progress";
 import { Button } from "./ui/button";
 
 export function CheckUpdater() {
-    const [update,setUpdate] = useState<Update>();
+    const [update, setUpdate] = useState<Update>();
     const [showUpdateDialog, setShowUpdateDialog] = useState(false);
     const [isUpdating, setIsUpdating] = useState(false);
     const [updateProgress, setUpdateProgress] = useState(0);
     const [updateComplete, setUpdateComplete] = useState(false);
 
     useEffect(() => {
-         check().then(() => {
+        check().then(() => {
             if (update) {
                 setShowUpdateDialog(true);
                 setUpdate(update);
             }
-          }).catch((e) => console.log("err",e))
-         
+        }).catch((e) => console.log("err", e))
+
     }, []);
 
     const startUpdate = async () => {
-        if(!update) return ;
+        if (!update) return;
         setIsUpdating(true);
-                let downloaded = 0;
-                let contentLength = 0;
-                await update.downloadAndInstall((event) => {
-                    switch (event.event) {
-                        case 'Started':
-                            contentLength = event.data.contentLength!;
-                            break;
-                        case 'Progress':
-                            downloaded += event.data.chunkLength;
-                            setUpdateProgress(downloaded);
-                            break;
-                        case 'Finished':
-                             setUpdateComplete(true)
-                            break;
-                    }
-                });  
-                
-                 let progress = 0;
-                const updateInterval = setInterval(() => {
-                    progress += Math.floor(Math.random() * 8) + 3 // Random progress between 3-10%
-                    if (progress >= 100) {
-                        progress = 100
-                        clearInterval(updateInterval)
-                        setTimeout(() => {
-                            setUpdateComplete(true)
-                        }, 500)
-                    }
-                    setUpdateProgress(progress)
-                }, 300);   
-                console.log('update installed');
-               
+        let downloaded = 0;
+        await update.downloadAndInstall((event) => {
+            switch (event.event) {
+
+                case 'Progress':
+                    downloaded += event.data.chunkLength;
+                    setUpdateProgress(downloaded);
+                    break;
+                case 'Finished':
+                    setUpdateComplete(true)
+                    break;
+            }
+        });
+
+        let progress = 0;
+        const updateInterval = setInterval(() => {
+            progress += Math.floor(Math.random() * 8) + 3 // Random progress between 3-10%
+            if (progress >= 100) {
+                progress = 100
+                clearInterval(updateInterval)
+                setTimeout(() => {
+                    setUpdateComplete(true)
+                }, 500)
+            }
+            setUpdateProgress(progress)
+        }, 300);
+        console.log('update installed');
+
     }
 
     const restartApp = async () => {
@@ -69,19 +66,19 @@ export function CheckUpdater() {
             <DialogHeader>
                 <DialogTitle className="flex items-center gap-2">
                     <Download className="h-5 w-5 text-primary" />
-                   New version Available
+                    New version Available
                 </DialogTitle>
                 <DialogDescription>
-                      { (!isUpdating && !updateComplete) && (
-                          <span>A new version of BackPair (v1.1.0) is available.</span>
-                      )}
-                  
+                    {(!isUpdating && !updateComplete) && (
+                        <span>A new version of BackPair (v1.1.0) is available.</span>
+                    )}
+
                 </DialogDescription>
             </DialogHeader>
             {isUpdating && !updateComplete && (
                 <div className="space-y-4 py-4">
                     <div className="flex items-center gap-3 mb-2">
-                            <p className="text-sm text-muted-foreground">Please don't close the application.</p>
+                        <p className="text-sm text-muted-foreground">Please don't close the application.</p>
                     </div>
                     <Progress value={updateProgress} className="h-2" />
                     <p className="text-sm text-center text-muted-foreground">{updateProgress}% complete</p>
