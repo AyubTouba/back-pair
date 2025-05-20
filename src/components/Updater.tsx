19c89997f373c57e16c7e3512826db7cf5361277
Ayoub TOUBA <ayubtouba@gmail.com>
2025-05-20 17:12:50 +0200

    uncoment updater

--- src/components/Updater.tsx
+++ src/components/Updater.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-// import { check } from '@tauri-apps/plugin-updater';
+import { check, Update } from '@tauri-apps/plugin-updater';
 import { relaunch } from '@tauri-apps/plugin-process';
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "./ui/dialog";
 import { CheckCircle2, Download } from "lucide-react";
@@ -7,52 +7,41 @@ import { Progress } from "./ui/progress";
 import { Button } from "./ui/button";
 
 export function CheckUpdater() {
-    const [update,setUpdate] = useState<{ version: string; date: Date; body: string; }>();
+    const [update,setUpdate] = useState<Update>();
     const [showUpdateDialog, setShowUpdateDialog] = useState(false);
     const [isUpdating, setIsUpdating] = useState(false);
     const [updateProgress, setUpdateProgress] = useState(0);
     const [updateComplete, setUpdateComplete] = useState(false);
 
     useEffect(() => {
-        const checkUpdate = async () => {
-           // const update = await check();
-           const update = {version:"0.3.1",date:new Date(),body:"TEST"};
-            console.log(update);
+         check().then(() => {
             if (update) {
                 setShowUpdateDialog(true);
                 setUpdate(update);
             }
-        }
-        checkUpdate();
+          }).catch((e) => console.log("err",e))
+         
     }, []);
 
     const startUpdate = async () => {
         if(!update) return ;
         setIsUpdating(true);
-        
-         console.log(
-                    `found update ${update.version} from ${update.date} with notes ${update.body}`
-                );
-                // let downloaded = 0;
-                // let contentLength = 0;
-                // alternatively we could also call update.download() and update.install() separately
-                // await update.downloadAndInstall((event) => {
-                //     switch (event.event) {
-                //         case 'Started':
-                //             contentLength = event.data.contentLength!;
-                //             console.log(`started downloading ${event.data.contentLength} bytes`);
-                //             break;
-                //         case 'Progress':
-                //             downloaded += event.data.chunkLength;
-                //             console.log(`downloaded ${downloaded} from ${contentLength}`);
-                //             setUpdateProgress(downloaded);
-                //             break;
-                //         case 'Finished':
-                //              setUpdateComplete(true)
-                //             console.log('download finished');
-                //             break;
-                //     }
-                // });  
+                let downloaded = 0;
+                let contentLength = 0;
+                await update.downloadAndInstall((event) => {
+                    switch (event.event) {
+                        case 'Started':
+                            contentLength = event.data.contentLength!;
+                            break;
+                        case 'Progress':
+                            downloaded += event.data.chunkLength;
+                            setUpdateProgress(downloaded);
+                            break;
+                        case 'Finished':
+                             setUpdateComplete(true)
+                            break;
+                    }
+                });  
                 
                  let progress = 0;
                 const updateInterval = setInterval(() => {
